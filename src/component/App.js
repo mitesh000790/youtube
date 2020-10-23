0c281cdde6ceba3415edee537a487988f8c22a38
root <mitesh000790@gmail.com>
2020-10-23 10:52:34 +0530

    add youtube demo clone

---
+++ src/component/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+//add yor api key.
+const KEY = 'your Api key'; 
+
+class App extends React.Component{
+    state = { videos : [] , selectedVideo :''};
+
+    componentDidMount(){
+        this.onTermSubmit('songs');
+     }
+
+    onTermSubmit = async term => {
+      const response = await youtube.get('/search',{
+       params:{
+        part: 'snippet',
+        q: term,
+        maxResults: 10,
+        key: KEY,       
+       }
+      })
+      this.setState({videos : response.data.items,
+                     selectedVideo:response.data.items[0]}) 
+    };
+
+    onVideoSelect = video =>{
+        this.setState({selectedVideo : video});
+       
+    }
+    render(){
+        return(
+            <div className="ui container" style={{marginTop:'10px'}}>
+               
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                             <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                             <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
